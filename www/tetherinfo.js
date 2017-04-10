5077cf52b50c20b4c2d2fb89143d52dc5c9dc158
Jonas Olmstead <sanojo@hotmail.com>
2017-04-10 11:47:31 +0200

    fix exec

--- www/tetherinfo.js
+++ www/tetherinfo.js
@@ -1,5 +1,13 @@
 /*global cordova, module*/
 
+
+'use strict';
+var exec = require('cordova/exec');
+
+var fail = function(o) {
+    console.error("Error " + JSON.stringify(o));
+};
+
 module.exports = {
   listip : function(success) {
       return exec(success, fail, "TetherInfo", "listip", [ ]);
