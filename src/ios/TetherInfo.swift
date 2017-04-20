33bc6833ff285142fad937c7b4961e7d3f46ce5e
Jonas Olmstead <sanojo@hotmail.com>
2017-04-20 10:42:09 +0200

    swift support

--- src/ios/TetherInfo.swift
+++ src/ios/TetherInfo.swift
@@ -46,32 +46,10 @@
 
     let msg = false
 
-    if msg.characters.count > 0 {
-      let toastController: UIAlertController =
-        UIAlertController(
-          title: "",
-          message: msg,
-          preferredStyle: .alert
-        )
-
-      self.viewController?.present(
-        toastController,
-        animated: true,
-        completion: nil
-      )
-
-      DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-        toastController.dismiss(
-          animated: true,
-          completion: nil
-        )
-      }
-
-      pluginResult = CDVPluginResult(
-        status: CDVCommandStatus_OK,
-        messageAs: msg
-      )
-    }
+    pluginResult = CDVPluginResult(
+      status: CDVCommandStatus_OK,
+      messageAs: msg
+    )
 
     self.commandDelegate!.send(
       pluginResult,
