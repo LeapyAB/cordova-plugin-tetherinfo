fba06f7b8136d64bc0b3600ec41904002c37fcd9
Jonas Olmstead <sanojo@hotmail.com>
2017-04-20 10:15:19 +0200

    start ios support

---
+++ src/ios/TetherInfo.swift
@@ -0,0 +1,81 @@
+@objc(TetherInfo) class TetherInfo : CDVPlugin {
+  @objc(echo:)
+  func listip(command: CDVInvokedUrlCommand) {
+    var pluginResult = CDVPluginResult(
+      status: CDVCommandStatus_ERROR
+    )
+
+    let msg = "hello"
+
+    if msg.characters.count > 0 {
+      let toastController: UIAlertController =
+        UIAlertController(
+          title: "",
+          message: msg,
+          preferredStyle: .alert
+        )
+
+      self.viewController?.present(
+        toastController,
+        animated: true,
+        completion: nil
+      )
+
+      DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+        toastController.dismiss(
+          animated: true,
+          completion: nil
+        )
+      }
+
+      pluginResult = CDVPluginResult(
+        status: CDVCommandStatus_OK,
+        messageAs: msg
+      )
+    }
+
+    self.commandDelegate!.send(
+      pluginResult,
+      callbackId: command.callbackId
+    )
+  }
+  func checkusb(command: CDVInvokedUrlCommand) {
+    var pluginResult = CDVPluginResult(
+      status: CDVCommandStatus_ERROR
+    )
+
+    let msg = false
+
+    if msg.characters.count > 0 {
+      let toastController: UIAlertController =
+        UIAlertController(
+          title: "",
+          message: msg,
+          preferredStyle: .alert
+        )
+
+      self.viewController?.present(
+        toastController,
+        animated: true,
+        completion: nil
+      )
+
+      DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+        toastController.dismiss(
+          animated: true,
+          completion: nil
+        )
+      }
+
+      pluginResult = CDVPluginResult(
+        status: CDVCommandStatus_OK,
+        messageAs: msg
+      )
+    }
+
+    self.commandDelegate!.send(
+      pluginResult,
+      callbackId: command.callbackId
+    )
+  }
+}
