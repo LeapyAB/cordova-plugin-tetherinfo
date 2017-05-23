3e14de2cf080a2a6241585b1b64a73e15f6e5736
Jonas Olmstead <sanojo@hotmail.com>
2017-05-23 11:56:16 +0200

    test param

--- src/ios/TetherInfo.swift
+++ src/ios/TetherInfo.swift
@@ -35,11 +35,11 @@ import SystemConfiguration.CaptiveNetwork
     let interfaces:CFArray! = CNCopySupportedInterfaces()
     if interfaces != nil {
       for i in 0..<CFArrayGetCount(interfaces){
-        let interfaceName = CFArrayGetValueAtIndex(interfaces, i)
+        let interfaceName: UnsafeRawPointer = CFArrayGetValueAtIndex(interfaces, i)
         let rec = unsafeBitCast(interfaceName, to: AnyObject.self)
         let unsafeInterfaceData = CNCopyCurrentNetworkInfo("\(rec)" as CFString)
         if unsafeInterfaceData != nil {
-          let interfaceData = unsafeInterfaceData! as Dictionary!
+          let interfaceData = unsafeInterfaceData! as NSDictionary
           currentSSID = interfaceData["SSID"] as! String
         }
       }
